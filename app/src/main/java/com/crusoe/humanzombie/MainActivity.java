359508af16cb9da84a1ef8247dcf1dfdd14651a3
Logan Allen <loganallc@gmail.com>
2014-10-04 09:20:47 -0700

    Working push notifications for real now.

---
+++ app/src/main/java/com/crusoe/humanzombie/MainActivity.java
@@ -0,0 +1,52 @@
+package com.crusoe.humanzombie;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.parse.ParseAnalytics;
+import com.parse.ParseInstallation;
+import com.parse.ParsePush;
+
+import java.util.List;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ParseAnalytics.trackAppOpened(getIntent());
+
+        List<String> subscribedChannels = ParseInstallation.getCurrentInstallation().getList("channels");
+
+        for (String i : subscribedChannels) {
+            Log.d("channels", i);
+            //Log.d("", subscribedChannels.get(i));
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/crusoe/humanzombie/ParseApplication.java
@@ -0,0 +1,36 @@
+package com.crusoe.humanzombie;
+
+
+import android.app.Application;
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseInstallation;
+import com.parse.ParsePush;
+import com.parse.ParseUser;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.setLogLevel(Parse.LOG_LEVEL_VERBOSE);
+
+        Parse.initialize(this, "v4bNZol2bilO5zypEAw7utrlB97XadvqCWrWBzaw", "ZgQQxUNWdVZE7wo59g3RmXU9881zOEmwT1uAaqno");
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+
+        ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+
+        // If you would like all objects to be private by default, remove this line.
+        defaultACL.setPublicReadAccess(true);
+
+        ParseACL.setDefaultACL(defaultACL, true);
+
+        ParsePush.subscribeInBackground("Humans");
+
+        ParseInstallation installation = ParseInstallation.getCurrentInstallation();
+        installation.put("user", ParseUser.getCurrentUser());
+        installation.saveInBackground();
+    }
+}
