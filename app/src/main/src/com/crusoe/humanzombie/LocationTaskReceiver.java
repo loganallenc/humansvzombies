05ef6e9c67492af5ceccf71e96fda9f64f4010de
edbert <edbert.chan@yahoo.com>
2014-10-04 15:39:29 -0700

    Merged Zombie activity changes
    Edbert’s Stuff

---
+++ app/src/main/src/com/crusoe/humanzombie/LocationTaskReceiver.java
@@ -0,0 +1,56 @@
+package com.crusoe.humanzombie;
+
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class LocationTaskReceiver extends BroadcastReceiver implements LocationListener {
+	protected LocationManager locationManager;
+	protected LocationListener locationListener;
+    @Override
+    public void onReceive(Context arg0, Intent arg1) {
+        // For our recurring task, we'll just display a message
+      //  Toast.makeText(arg0, "I'm running", Toast.LENGTH_SHORT).show();
+    	
+    	locationManager = (LocationManager) arg0.getSystemService(Context.LOCATION_SERVICE);
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,
+				0, this);
+		Log.i("LocationTaskReceiver", "Location Request");
+    }
+	@Override
+	public void onLocationChanged(Location location) {
+		// TODO Auto-generated method stub
+
+		latitude = location.getLatitude();
+		longitude = location.getLongitude();
+		// TODO Auto-generated method stub
+		Log.i("Latitude", Double.toString(latitude));
+		Log.i("Longitude", Double.toString(longitude));
+	}
+	double latitude = 0;
+	double longitude = 0;
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+	
+		
+	}
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ app/src/main/src/com/crusoe/humanzombie/MainActivity.java
@@ -0,0 +1,99 @@
+package com.crusoe.humanzombie;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.parse.FindCallback;
+import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseInstallation;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+
+public class MainActivity extends Activity {
+
+	ParseUser userObject;
+	ParseGeoPoint geo;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		ParseAnalytics.trackAppOpened(getIntent());
+
+		userObject = ParseUser.getCurrentUser();
+
+		List<String> subscribedChannels = ParseInstallation
+				.getCurrentInstallation().getList("channels");
+
+		for (String i : subscribedChannels) {
+			Log.d("channels", i);
+		}
+
+		nearMe();
+	}
+
+	public void nearMe() {
+		GPSTracker gps = new GPSTracker(this);
+		double latitude = gps.getLatitude();
+		double longitude = gps.getLongitude();
+
+		geo = new ParseGeoPoint(latitude, longitude);
+
+		// geo = user.getParseGeoPoint("location");
+		userObject.put("location", geo);
+		userObject.saveEventually();
+
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("location");
+		query.whereWithinMiles("location", geo, 0.5);
+		query.setLimit(10);
+		query.findInBackground(new FindCallback<ParseObject>() {
+
+			@Override
+			public void done(List<ParseObject> parseObjects, ParseException e) {
+				if (e == null) {
+					for (ParseObject p : parseObjects) {
+						Log.d("near", p.getObjectId());
+					}
+				} else {
+					System.out.println(e);
+				}
+			}
+		});
+	}
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	public void launchHuman(View v) {
+		Intent intent = new Intent(this, HumanActivity.class);
+		startActivity(intent);
+	}
+
+	public void launchZombie(View v) {
+		Intent intent = new Intent(this, ZombieActivity.class);
+		startActivity(intent);
+	}
+}
